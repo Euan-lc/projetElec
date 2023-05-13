03f46ae2c4a5fe5e97df6a65a2c1fe7283a0db2d
JHespelLouis <93406828+JHespelLouis@users.noreply.github.com>
2023-05-13 20:19:55 +0200

    Circles to display led status

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ class App(tk.Tk):
     def __init__(self, com_port):
         super().__init__()
 
+        self.led_max_distance = 0
+
         self.serial_connection = serial.Serial(
             port=com_port,
             baudrate=9600,
@@ -13,6 +15,8 @@ class App(tk.Tk):
             stopbits=serial.STOPBITS_ONE
         )
 
+
+
         self.label = tk.Label(self, font=("Arial", 20), fg="blue")
         self.title("App Proj Elec")
 
@@ -20,7 +24,7 @@ class App(tk.Tk):
         self.resizable(False, False)
 
         # Taille de la fenêtre
-        self.geometry("350x200")
+        self.geometry("350x125")
 
         # Texte dessus
         self.label_distance = tk.Label(self, text="Indiquez la distance voulue", font=("Arial", 13))
@@ -41,10 +45,18 @@ class App(tk.Tk):
         self.distance_label = tk.Label(self, text="Vous êtes à une distance inconnue", font=("Arial", 13))
         self.distance_label.place(x=8, y=70, width=275, height=30)
 
-        self.after(0, self.update_display)
-        self.mainloop()
+        # Cercle rouge
+        self.canvas = tk.Canvas(self)
+        self.red_circle = self.canvas.create_oval(10, 10, 50, 50, fill="#FFC0CB")
+        self.canvas.place(x=290, y=65, width=60, height=60)
 
-        # threading.Thread(target=self.mainloop()).start()
+        # Cercle vert
+        self.canvas2 = tk.Canvas(self)
+        self.green_circle = self.canvas2.create_oval(10, 10, 50, 50, fill="#BDFCC9")
+        self.canvas2.place(x=290, y=10, width=60, height=60)
+
+        #self.after(0, self.update_display)
+        self.mainloop()
 
     def valider_entree(self, action, valeur_entree):
         # Vérifier si le texte entré est un nombre
@@ -54,20 +66,29 @@ class App(tk.Tk):
         return True
 
     def send(self, **kwargs):
-        distance = int(self.entry.get())
-        self.serial_connection.write(str.encode(str(distance) + "\r\n"))
+        self.led_max_distance = int(self.entry.get())
+        self.serial_connection.write(str.encode(str(self.led_max_distance) + "\r\n"))
 
     def read_serial(self):
         data = self.serial_connection.readline()
         try:
             return int(data.strip())
-        except:
-            print(data)
+        except ValueError:
+            return 0
 
     def update_display(self):
         data = self.read_serial()
         self.distance_label.config(text="Vous êtes à une distance de "+str(data)+" cm")
+        self.update_led_display(data)
         self.after(100, self.update_display)
+
+    def update_led_display(self, distance):
+        if distance > self.led_max_distance:
+            self.canvas.itemconfig(self.green_circle, fill="#BDFCC9")
+            self.canvas.itemconfig(self.red_circle, fill="red")
+        else:
+            self.canvas.itemconfig(self.green_circle, fill="green")
+            self.canvas.itemconfig(self.red_circle, fill="#FFC0CB")
 
 
 if __name__ == "__main__":
